cad6cd2aa7c88f8dc4b1355e83a7b91e2a83decb
Tanmay Adithya <marnitanmayadithya@gmail.com>
2023-08-01 11:01:28 +0530

    feat: Refactor navigation links and add dynamic routing
    In this commit, I made some significant improvements to the navigation links and added dynamic routing to the React application.
    
    Changes Made:
    
        Refactor Navigation Links:
            The previous code had static navigation links with href attributes. I updated it to use more semantic and reusable data objects for navigation items.
            Created a navigation array that contains objects with the properties name, path, and description. This provides better readability and maintainability.
    
        Dynamic Routing with React Router:
            Imported Link from react-router-dom to enable dynamic routing.
            Converted the previous anchor tags to Link components, passing the appropriate to prop with the corresponding path from the navigation array.
            Utilized the description property from the navigation array to display helpful descriptions for each navigation item.
    
        Cleaned up Redundant Code:
            Removed unused imports such as Bars3Icon, XMarkIcon, and RoutesContainer.
            Fixed a missing closing tag for <div className="flex lg:flex-1">.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import '../src/App.css'
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import "../src/App.css";
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import RoutesContainer from "../src/components/RoutesContainer";
+import { Link } from "react-router-dom";
 
 const navigation = [
-  { name: 'Resources', href: '#' },
-  { name: "FAQ's", href: '#' },
-  { name: 'Doc Maker', href: '#' },
-]
+  {
+    name: "Resources",
+    path: "/Resources",
+    description: "Explore resources which come in handy at hackathons",
+  },
+  {
+    name: "Tips",
+    path: "/Tips",
+    description: "Tips for getting the most out of your hackathon",
+  },
+  {
+    name: "DocMaker",
+    path: "/DocMaker",
+    description: "Create a detailed documentation of your project online",
+  },
+];
 
 export default function App() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -16,7 +30,10 @@ export default function App() {
     <>
       <div className="bg-white">
         <header className="absolute inset-x-0 top-0 z-50">
-          <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+          <nav
+            className="flex items-center justify-between p-6 lg:px-8"
+            aria-label="Global"
+          >
             <div className="flex lg:flex-1">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Hack Pack</span>
@@ -39,13 +56,22 @@ export default function App() {
             </div>
             <div className="hidden lg:flex lg:gap-x-12">
               {navigation.map((item) => (
-                <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="text-sm font-semibold leading-6 text-gray-900"
+                >
                   {item.name}
                 </a>
               ))}
             </div>
           </nav>
-          <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+          <Dialog
+            as="div"
+            className="lg:hidden"
+            open={mobileMenuOpen}
+            onClose={setMobileMenuOpen}
+          >
             <div className="fixed inset-0 z-50" />
             <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
               <div className="flex items-center justify-between">
@@ -94,7 +120,7 @@ export default function App() {
               className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
               style={{
                 clipPath:
-                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
               }}
             />
           </div>
@@ -104,7 +130,8 @@ export default function App() {
                 The Pandora&apos;s Box you need at a hackathon
               </h1>
               <p className="font-geologica font-normal mt-6 text-lg leading-8 text-gray-600">
-                Your Ultimate Arsenal of Resources and Tips to Crush It at Hackathons!
+                Your Ultimate Arsenal of Resources and Tips to Crush It at
+                Hackathons!
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
@@ -113,7 +140,10 @@ export default function App() {
                 >
                   Get started
                 </a>
-                <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                <a
+                  href="#"
+                  className="text-sm font-semibold leading-6 text-gray-900"
+                >
                   Learn more <span aria-hidden="true">→</span>
                 </a>
               </div>
@@ -127,7 +157,7 @@ export default function App() {
               className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
               style={{
                 clipPath:
-                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
               }}
             />
           </div>
@@ -137,63 +167,46 @@ export default function App() {
         <div className="bg-white py-24 sm:py-32">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl sm:text-center">
-              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Simple no-tricks pricing</h2>
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                Simple no-tricks pricing
+              </h2>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et quasi iusto modi velit ut non voluptas
-                in. Explicabo id ut laborum.
+                Distinctio et nulla eum soluta et neque labore quibusdam. Saepe
+                et quasi iusto modi velit ut non voluptas in. Explicabo id ut
+                laborum.
               </p>
             </div>
             <div className="mx-auto mt-16 max-w-2xl rounded-3xl sm:mt-20 lg:mx-0 lg:max-w-none">
               <div className="-mt-2 p-2 lg:mt-0 lg:flex lg:justify-around lg:w-full lg:flex-shrink-0">
-                <div className="mx-2 mt-4 ounded-2xl bg-gray-50 py-5 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-1/3 lg:justify-center lg:py-16">
-                  <div className="mx-auto max-w-xs px-8">
-                    <p className="mt-6 flex items-baseline justify-center gap-x-2">
-                      <span className="text-5xl font-bold tracking-tight text-gray-900">Resources</span>                    </p>
-                    <a
-                      href="#"
-                      className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                      Explore
-                    </a>
-                    <p className="mt-6 text-xs leading-5 text-gray-600">
-                      Invoices and receipts available for easy company reimbursement
-                    </p>
+                {navigation.map((item) => (
+                  <div
+                    key={item.name}
+                    className="mx-2 mt-4 ounded-2xl bg-gray-50 py-5 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-1/3 lg:justify-center lg:py-16"
+                  >
+                    <div className="mx-auto max-w-xs px-8">
+                      <p className="mt-6 flex items-baseline justify-center gap-x-2">
+                        <span className="text-3xl font-bold tracking-tight text-gray-900">
+                          {item.name}
+                        </span>
+                      </p>
+                      <Link
+                        to={item.path}
+                        className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      >
+                        Explore
+                      </Link>
+                      <p className="mt-6 text-xs leading-5 text-gray-600">
+                        {item.description}
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="mx-2 mt-4 rounded-2xl bg-gray-50 py-5 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-1/3 lg:justify-center lg:py-16">
-                  <div className="mx-auto max-w-xs px-8">
-                    <p className="mt-6 flex items-baseline justify-center gap-x-2">
-                      <span className="text-5xl font-bold tracking-tight text-gray-900">Tips</span>                    </p>
-                    <a
-                      href="#"
-                      className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                      Explore
-                    </a>
-                    <p className="mt-6 text-xs leading-5 text-gray-600">
-                      Invoices and receipts available for easy company reimbursement
-                    </p>
-                  </div>
-                </div>
-                <div className="mx-2 mt-4 rounded-2xl bg-gray-50 py-5 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-1/3 lg:justify-center lg:py-16">
-                  <div className="mx-auto max-w-xs px-8">
-                    <p className="mt-6 flex items-baseline justify-center gap-x-2">
-                      <span className="text-5xl font-bold tracking-tight text-gray-900">Guides</span>                    </p>
-                    <a
-                      href="#"
-                      className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                      Explore</a>
-                    <p className="mt-6 text-xs leading-5 text-gray-600">
-                      Invoices and receipts available for easy company reimbursement
-                    </p>
-                  </div>
-                </div>
+                ))}
+                <RoutesContainer />
               </div>
             </div>
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
